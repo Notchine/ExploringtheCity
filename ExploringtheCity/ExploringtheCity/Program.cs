f7a22f71dd4d3025b7ebfd5150e3ac7c98cca361
Not <aelmiet@hotmail.com>
2021-05-06 10:30:14 +0100

    A LOT OF SHIT LIKE THE BLACK MARKET ACCESS HAS BEEN FIXED

--- ExploringtheCity/ExploringtheCity/Program.cs
+++ ExploringtheCity/ExploringtheCity/Program.cs
@@ -10,6 +10,7 @@ public class Program
 	//public static string[] inventory = new string[10];
 	public static ArrayList inventory2 = new ArrayList();
 	public static bool thiefSpawn = true;
+	public static bool blackMarketKnowledge = true;
 	public struct Player
 	{
 		public int PlayerHealth;
@@ -19,7 +20,18 @@ public class Program
 		public int EXPToNextLVL;
 		public int EXP;
 	}
-	public static Player p1;
+	#region levelling
+	//	void LevelUp()
+	//    {
+	//		Player.level++
+	//			Player.xp = 0
+	//levelling shit
+
+	//		{ex limit} *= 1.2
+	//increases required exp for each level by 20%
+//}
+#endregion
+public static Player p1;
 
 	public struct Enemy
 	{
@@ -32,21 +44,20 @@ public class Program
 	public static Enemy thief;
 	public static Enemy drugAddict;
 
-	struct Weapon
+	public struct Weapon
 	{
 		#region weapon data
 		public int id;
 		public string weaponName;
-
-		public int Dmg { get; set; }
-
-		public int critChance { get; set; }
-
 		public string weaponInfo { get; set; }
-
-		public int[] attacks;
+		public int Dmg { get; set; }
+		public int critChance { get; set; }
 		#endregion
 	}
+	public static Weapon CrackedBaton;
+	public static Weapon Handgun;
+	public static Weapon Uzi;
+
 	static public string deathText = "You have now DIED. X_X";
     public static void Combat(Enemy enemy)
 	{
@@ -57,6 +68,10 @@ public class Program
 			if (enemy.EnemyHealth <= 0)
 			{
 				Console.WriteLine("Enemy Defeated!");
+				p1.EXP += enemy.EnemyEXP;
+				Console.WriteLine("Earned EXP: "+ enemy.EnemyEXP);
+				Console.WriteLine("Total Gained Experience: "+ p1.EXP);
+				Console.WriteLine("EXP to next level: " + p1.EXPToNextLVL);
 				//write Exp earned and whether they level up maybe
 				enemy.EnemyHealth = +enemy.EnemyMaxHP;
 				break;
@@ -92,11 +107,12 @@ public class Program
 
 		
 		//description of room
-		Console.WriteLine("You awaken in a dark alley with no recollection of how you got there. There is a faint smell of oil and old garbage surrounding you. The only exit you can see if the opening at the end of the alleyway which shines with bright, blinding streetlights.");
-
+		Console.WriteLine("You are standing in a dark alley with no real recollection of how you got there. There is a faint smell of oil and old garbage surrounding you. The only exit you can see if the opening at the end of the alleyway which shines with bright, blinding streetlights.");
+		Console.WriteLine(" ");
 		//player options
 		Console.WriteLine("1. Walk out the alleyway");
 		Console.WriteLine("2. Stay in the alleyway");
+		Console.WriteLine(" ");
 
 		//user input
 		int input = Convert.ToInt32(Console.ReadLine());
@@ -139,76 +155,112 @@ public class Program
 
 
 		//description of room
+		Console.WriteLine(" ");
 		Console.WriteLine("You are now standing in the middle of the busy sidewalk of the city, the faceless crowd pushing and shoving past you. You notice a PECULIAR LOCKED BOX chained shut which has a small keyhole in front.");
-
+		Console.WriteLine(" ");
 		//player options
 		Console.WriteLine("1. Investigate the PECULIAR LOCKED BOX");
 		Console.WriteLine("2. Return to the alleyway");
 		Console.WriteLine("3. Continue down the street.");
+		Console.WriteLine(" ");
 
 		//user input
 		int input = Convert.ToInt32(Console.ReadLine());
-		//condition check to move rooms
-		switch (input)
-		{
-			case 1:
-				if (inventory2.Contains("Small Key"))
-				{
-					Console.WriteLine("After unlocking the PECULIAR LOCKED BOX, the oversized padlock as well as the chains tightly wrapped around it thud to the ground, generating a sound so loud it could be heard even beyond the sounds of the bustling city streets.");
+			//condition check to move rooms
+			switch (input)
+			{
+				case 1:
+					if (inventory2.Contains("Small Key"))
+					{
+						Console.WriteLine("After unlocking the PECULIAR LOCKED BOX, the oversized padlock as well as the chains tightly wrapped around it thud to the ground, generating a sound so loud it could be heard even beyond the sounds of the bustling city streets.");
 
-					Console.WriteLine("After opening the PECULIAR LOCKED BOX, you discover the SMALL CRACKED BATON and place it into your inventory. You also throw the SMALL KEY away, as you doubt you'll get much usage out of it anymore.");
-					inventory2.Add("Small Cracked Baton");
-				}
-				else
-				{
-					Console.WriteLine("It seems to be chained shut and cannot be opened right now.");
-					Console.WriteLine();
-					Room1();
-				}
-				break; //breaks out of this entire case, moving  on the next part of da code
-			case 2:
-				Console.WriteLine("You turn around and begin walking towards the alleyway you were previously in.");
-				StartingRoom();
-				break;
-			case 3:
-				Console.WriteLine("You make your way down the city streets...");
-				Room2();
-				break;
-			default:
-				Console.WriteLine("Invalid entry, try again.");
-				break;
+						Console.WriteLine("After opening the PECULIAR LOCKED BOX, you discover the SMALL CRACKED BATON and place it into your inventory. You also throw the SMALL KEY away, as you doubt you'll get much usage out of it anymore.");
+						inventory2.Add("Small Cracked Baton");
+					}
+					else
+					{
+						Console.WriteLine("It seems to be chained shut and cannot be opened right now.");
+						Console.WriteLine();
+						Room1();
+					}
+					break; //breaks out of this entire case, moving  on the next part of da code
+				case 2:
+					Console.WriteLine("You turn around and begin walking towards the alleyway you were previously in.");
+					StartingRoom();
+					break;
+				case 3:
+					Console.WriteLine("You make your way down the city streets...");
+					Room2();
+					break;
+				default:
+					Console.WriteLine("Invalid entry, try again.");
+					break;
+			}
 		}
-	}
+
 	public static void Room2()
 	{
 		//description of room
+		Console.WriteLine(" ");
 		Console.WriteLine("You are now in the shopping district. This place seems even more bustling than the city streets, with even more people rushing past you as you try to find your way through the densely packed hoard.");
-
+		Console.WriteLine(" ");
 		//player options
 		Console.WriteLine("1. Walk to the city streets");
 		Console.WriteLine("2. Walk to the train station");
-		Console.WriteLine("3. enter the store");
-		//codeword for entering black market is "the stallion is shiny"
+		Console.WriteLine("3. Enter the store");
+		Console.WriteLine(" ");
+		//codeword for entering black market is "the stallion is shiny" ACTUALLY NO THIS IS DUMB JUST HAVE IT BE ENABLED BY BEATING THE DUDES IN THE ABANDONED SCHOOL I KNOW YOU CAN JUST HAVE IT CHECK FOR A SPECIFIC STATEMENT BUT A BOOLEAN IS INFINITELY EASIER YOU DUMMY
 		//use different switches, one for before you discover the black market and one for after. Use a static boolean and set it to true when youre ready to enter the black market.
-		//user input
-		int input = Convert.ToInt32(Console.ReadLine());
-		//condition check to move rooms
-		switch (input)
+		if (blackMarketKnowledge == false)
 		{
-			case 1:
-				Console.WriteLine("You make your way back to the city streets...");
-				Room1();
-				break; //breaks out of this entire case, moving  on the next part of da code
-			case 2:
-				Console.WriteLine("You make your way to the train station...");
-				Room3();
-				break;
-			case 3:
-				Console.WriteLine("You approach the store door, push it open, and begin to speak with the cashier...");
-				break;
-			default:
-				Console.WriteLine("Invalid entry, try again.");
-				break;
+			//user input
+			int input = Convert.ToInt32(Console.ReadLine());
+			//condition check to move rooms
+			switch (input)
+			{
+				case 1:
+					Console.WriteLine("You make your way back to the city streets...");
+					Room1();
+					break; //breaks out of this entire case, moving  on the next part of da code
+				case 2:
+					Console.WriteLine("You make your way to the train station...");
+					Room3();
+					break;
+				case 3:
+					Console.WriteLine("You approach the store door, push it open, and begin to speak with the cashier...");
+					break;
+				default:
+					Console.WriteLine("Invalid entry, try again.");
+					break;
+			}
+		}
+		else if (blackMarketKnowledge == true)
+		{
+			Console.WriteLine("4. Descend to the black market");
+			//user input
+			int input = Convert.ToInt32(Console.ReadLine());
+			//condition check to move rooms
+			switch (input)
+			{
+				case 1:
+					Console.WriteLine("You make your way back to the city streets...");
+					Room1();
+					break; //breaks out of this entire case, moving  on the next part of da code
+				case 2:
+					Console.WriteLine("You make your way to the train station...");
+					Room3();
+					break;
+				case 3:
+					Console.WriteLine("You approach the store door, push it open, and begin to speak with the cashier...");
+					break;
+				case 4:
+					Console.WriteLine("You carefully step through a secluded entrance you previously did not notice, descending deeper into the shopping district away from the bustling humanity behind you...");
+					Room8(); //this is gonna be the black market
+					break;
+				default:
+					Console.WriteLine("Invalid entry, try again.");
+					break;
+			}
 		}
 	}
 	public static void Room3()
@@ -415,6 +467,7 @@ public class Program
 		thief.EnemyHealth = 4;
 		thief.EnemyMaxHP = 4;
 		thief.EnemyEXP = 3;
+		drugAddict.EnemyName = "Drug Addict";
 		drugAddict.EnemyAttack = 2;
 		drugAddict.EnemyHealth = 6;
 		drugAddict.EnemyMaxHP = 6;
